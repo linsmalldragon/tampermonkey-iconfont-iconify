fa62ce1bc049fe698b70ef4e186e8d4809340a93
linxiaolong <88277648@163.com>
2024-07-12 11:13:38 +0800

    info信息增加

--- src/index.ts
+++ src/index.ts
@@ -184,11 +184,11 @@ function makeIconifyJson(prefix, icons) {
     icons,
     width: 1024,
     height: 1024,
-    "info": {
-		"name": prefix,
-		"total": icons.length,
-		"samples": Object.keys(icons).slice(0, 3),
-		"category": "Custom"
+    info: {
+		name: prefix,
+		total: icons.length,
+		samples: Object.keys(icons).slice(0, 3),
+		category: "Custom"
 	},
   };
 }
